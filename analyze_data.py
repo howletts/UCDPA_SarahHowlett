e132eacb3372518b9455ad9bc86393bff214ee9d
howletts <howlett.sarah@gmail.com>
2021-04-24 01:11:18 +0100

    Add to project

--- analyze_data.py
+++ analyze_data.py
@@ -1,113 +1,116 @@
 import import_data as data
 import pandas as pd
+import numpy as np
 
 df_surf_act = data.df_surf_activities
 df_attractions = data.df_attractions
 df_accommodation = data.df_accommodation
-df_county_in_provence = data.df_county_province
-df_region_type = data.df_area_type[data.df_area_type['Council'] == 'City'][["Core County", "Council"]]
+df_county_in_province = data.df_county_province
+# Having sorted the DataFrame, iloc is used as the exact rows and cols of interest are known
+df_region_type = data.df_area_type.sort_values('Council').iloc[5:10,1:3]
 
-# create a reusable function to extract the value of the 'addressRegion' from a dictionary
+
+# reusable function to extract the value of the 'addressRegion' from a dictionary
 def extract_region(address):
     address_region = address['addressRegion']
     return address_region
 
+
+# reusable function to create a dictionary from a DataFrame and lookup the dictionary using the region passed as the key
+def get_province(value):
+    province_lkup = df_county_in_province.to_dict()['province']
+    return province_lkup[value]
+
+def get_lollipop_colours(df_col):
+    # Example of using python list comprehsion
+    colours = []
+    [colours.append('r') if value == 'City' else colours.append('g') for value in df_col.to_list()]
+    return colours
+
+
 ########################################
-#  Build dataframe for figure 1
+#  1. Analyse and process the activities dataset, specifically for venues suitable for all 3 surfing types
 ########################################
-# extract the address_region from the 'address' column for joining
+# extract the 'address_region' (county) from the 'address' column using a reusable custom function, add as new column
 df_surf_act['address_region'] = df_surf_act['address'].apply(extract_region)
-df_surf_act = df_surf_act.sort_values(["name", "address_region"], ascending=True)
-df_surf_act = df_surf_act.drop_duplicates(["name", "address_region"], keep='last')
-df_surf_act = df_surf_act.loc[:, ["name", "address_region", "tags"]]
-df_surf_venue_per_region = df_surf_act.groupby(["address_region"], as_index=False).agg(count_venue=pd.NamedAgg(column="name", aggfunc="count"))
+# sort the data by 'name' and 'address_region' (county) in ascending order
+df_surf_act = df_surf_act.sort_values(['name', 'address_region'], ascending=True)
+# remove records that are duplicate on 'name' and 'address_region', where there are many keep the last
+df_surf_act = df_surf_act.drop_duplicates(['name', 'address_region'], keep='last')
+# use .loc to select 'name', 'address_region', 'tags' columns only
+df_surf_act = df_surf_act.loc[:, ['name', 'address_region', 'tags']]
+# count the number of rows per 'address_region' (county) and save in a new dataframe df_surf_venue_per_region
+df_surf_venue_per_region = df_surf_act.groupby(['address_region'], as_index=False).agg(count_venue=pd.NamedAgg(column='name', aggfunc='count'))
+# left join with the df_region_type DafaFrame which contains a list of counties that have cities, use columns with different names to join on
 df_surf_per_cnty = df_surf_venue_per_region.merge(df_region_type, how = 'left', left_on=['address_region'], right_on=['Core County'])
-df_surf_per_cnty[["Council"]] = df_surf_per_cnty[["Council"]].fillna(value = "No City")
+# there are only 5 counties with cities so fill the empty fields with the value 'No City'
+df_surf_per_cnty[['Council']] = df_surf_per_cnty[['Council']].fillna(value = 'No City')
 
+########################################
+#  2. Analyse and process the attractions dataset
+########################################
 
+# drop records with duplicate 'Name' and 'AddressRegion', keep lats record in case of duplicates
+df_attractions = df_attractions.drop_duplicates(['Name', 'AddressRegion'], keep='last')
+# drop records that don't have an 'AddressRegion' as this field is needed for analysis
+df_attractions = df_attractions.dropna(subset = ['AddressRegion'])
+# fill any other empty cells in the other columns with 'Unknown'
+if df_attractions.isnull().values.any():
+    df_attractions = df_attractions.fillna(value = 'Unknown')
+# select only the 'Name', 'AddressRegion', 'Tags' columns using the .loc
+df_attractions = df_attractions.loc[:, ['Name', 'AddressRegion', 'Tags', 'Url', 'Telephone']]
+# sort the dataframe by 'Name', 'AddressRegion'
+df_attractions = df_attractions.sort_values(['Name', 'AddressRegion'], ascending=True)
+# Get any 'Name'/'AddressRegion' pair dups and remove them if they exist keeping the last record
+df_attractions = df_attractions.drop_duplicates(['Name', 'AddressRegion'], keep='last')
+# groupby 'AddressRegion' and get the count per County ('AddressRegion'), has index as default so keep it
+df_agg_attractions = df_attractions.groupby(['AddressRegion']).agg(number_attractions=pd.NamedAgg(column='Name', aggfunc='count'))
+# set the index to 'county' in df_county_in_province for joining efficiency
+df_county_in_province = df_county_in_province.set_index('county')
+# left join the df_attractions_count and df_county_in_province so every row on df_attractions_count remains
+df_agg_attractions = df_agg_attractions.merge(df_county_in_province, how = 'left', left_index = True, right_index = True)
 
-
-
-
-# get distinct tags in tag list
-def count_tags(tags):
-    #tags = ['Accommodation', 'Hotel', 'Follow the Shamrock', 'Standard', 'Covid Safety Charter', 'Visit Dublin']
-    dist_tag_count = len(list(set(tags)))
-    return dist_tag_count
-
-# extract the address_region from the 'address' column for joining
-
-#df_accomodation['address_region'] = df_accomodation['address'].apply(extract_region)
-
-# sorting by "Name" first and then region "AddressRegion" and populate the df_attractions DataFrame
-#df_attractions = df_attractions.sort_values(["Name", "AddressRegion"], ascending=True)
-#df_accomodation = df_accomodation.sort_values(["name", "address_region"], ascending=True)
-
-
-
+########################################
+#  Analyse and process data for figure 3
+########################################
 
 # drop duplicates
-#df_attractions = df_attractions.drop_duplicates(["Name", "AddressRegion"], keep='last')
-#df_accomodation = df_accomodation.drop_duplicates(["Name", "AddressRegion"], keep='last')
-
-##fill na,
-
-## call count_tags
-
+df_accommodation = df_accommodation.drop_duplicates(["Name", "AddressRegion"], keep='last')
+# drop records that don't have an "AddressRegion" set, this field is needed for analysis
+df_accommodation = df_accommodation.dropna(subset = ["AddressRegion"])
 # select only the "Name", "AddressRegion", "Tags" columns using the .loc
-#df_attractions = df_attractions.loc[:, ["Name", "AddressRegion", "Tags"]]
-#df_accomodation = df_accomodation.loc[:, ["Name", "AddressRegion", "Tags"]]
+df_accommodation = df_accommodation.loc[:, ["Name", "AddressRegion"]]
+# sort the dataframe
+df_accommodation = df_accommodation.sort_values(["Name", "AddressRegion"], ascending=True)
+# groupby "AddressRegion" and get the count (Name column) per County ("AddressRegion"), has index as default
+df_accommodation["Province"] = df_accommodation["AddressRegion"].apply(get_province)
 
-#df_attractions.isnull().values.any()
-#df_accomodation.isnull().values.any()
+########################################
+#  Analyse and process data for figure 4
+########################################
 
-
-# groupby "AddressRegion" and get the count (Name column) per County ("AddressRegion")
-#df_attractions_per_cnty = df_attractions.groupby(["AddressRegion"]).agg(number_attractions=pd.NamedAgg(column="Name", aggfunc="count"))
-#df_accomodation_per_cnty = df_accomodation.groupby(["AddressRegion"]).agg(number_accomdations=pd.NamedAgg(column="Name", aggfunc="count"))
-# check if the County ("AddressRegion") is the index
-#print(df_attractions_per_cnty.index)
-
-
-
-# set the index to "county"
-#df_county_in_provence = df_county_in_provence.set_index("county")
-
-# merge the df_attractions_count and df_county_in_provence so every row on df_attractions_count remains on index
-#df_attractions_per_cnty = df_attractions_per_cnty.merge(df_county_in_provence, how = 'left', left_index = True, right_index = True)
-
-# Get any "Name"/"AddressRegion" pair dups and remove them if they exist keeping the last record
-##df_dup_attractions = df_attractions[df_attractions.duplicated(["Name", "AddressRegion"], keep=False)]
-#if not df_dup_attractions.empty:
-#    print("Attractions that are duplicated in the list" + str(df_dup_attractions))
+df_agg_accommodation = df_accommodation.groupby(["AddressRegion"]).agg(number_attractions=pd.NamedAgg(column="Name",
+                                                                            aggfunc="count"))
+df_agg_accommodation = df_agg_accommodation.merge(df_county_in_province, how = 'left', left_index = True, right_index = True)
 
 
-# For insight 1:
-#df_attractions_munster = df_attractions_per_cnty[df_attractions_per_cnty["province"] == "Munster"]
-#df_attractions_leinster = df_attractions_per_cnty[df_attractions_per_cnty["province"] == "Leinster"]
-#df_attractions_connacht= df_attractions_per_cnty[df_attractions_per_cnty["province"] == "Connacht"]
-#df_attractions_ulster = df_attractions_per_cnty[df_attractions_per_cnty["province"] == "Ulster"]
+df_attractions_leinster = df_agg_attractions[df_agg_attractions["province"] == "Leinster"]
+df_accommodation_leinster = df_agg_accommodation[df_agg_accommodation["province"] == "Leinster"]
 
+# use numpy to check for correlation between the lists - 0.97128864 means it is a strong positive correlation
+np.corrcoef(df_attractions_leinster["number_attractions"], df_accommodation_leinster["number_attractions"])
 
+# use numpy to
+dict_accommod_percent = {}
+total = np.sum(df_agg_accommodation["number_attractions"])
+array_accommod_counts = np.array(df_agg_accommodation["number_attractions"])
+
+array_accommod_percent = np.rint(((array_accommod_counts * 100) / total)).tolist()
+counties =  (df_agg_accommodation.index).tolist()
+for i, item in enumerate(counties):
+    dict_accommod_percent[item] = array_accommod_percent[i]
+
+a = 0
 
 
 
-
-#df_region_type = df_region_type.sort_values(["Council"], ascending=True)
-#df_region_type = df_region_type.reset_index(drop=True)
-# Slice in both directions, the first 3 columns and rows 5-9 to get counties that contain a city
-#df_cities = df_region_type.iloc[5:10,0:3]
-
-#create dictionary from dup_attractions datafrome
-
-#.is_unique
-
-
-
-# order by province
-#for activities take a dictiry and extract a region - put as column
-
-#attractions get count of attractions per regoin
-#attractions per province
-#attractions per is city to no is city
-#city.sort_index(axis=1, inplace=True)
--- visualize_data.py
+++ visualize_data.py
@@ -2,120 +2,99 @@ import analyze_data as data
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+
+
+################
+# Figure 1: Lollipop Chart plotting number of surfing venues per county'
+################
 df_surf = data.df_surf_per_cnty
-
-
-def get_lollipop_colours(df_col):
-    # Example of using python list comprehsion
-    colours = []
-    [colours.append('r') if value == 'City' else colours.append('g') for value in df_col.to_list()]
-    return colours
-
-
-fig, ax = plt.subplots(figsize=(16,10), dpi= 80)
-lolly_colours = get_lollipop_colours(df_surf["Council"])
+fig, ax = plt.subplots(figsize=(20,16), dpi= 80)
+lolly_colours = data.get_lollipop_colours(df_surf["Council"])
 ax.vlines(x=df_surf["address_region"], ymin=0, ymax=df_surf["count_venue"], color=lolly_colours, alpha=0.7, linewidth=2)
-ax.scatter(x=df_surf["address_region"], y=df_surf["count_venue"], s=75, color=lolly_colours, alpha=0.7)
-ax.set_title('Figure 1: Lollipop Chart plotting number of surfing venues per county', fontdict={'size':22})
+scatter = ax.scatter(x=df_surf["address_region"], y=df_surf["count_venue"], s=75, color=lolly_colours, alpha=0.7)
+#legend1 = ax.legend(*scatter.legend_elements(), loc="lower left", title="Classes")
+#ax.add_artist(legend1)
+ax.set_title('Figure 1: Lollipop Chart plotting number of surfing venues per county (Counties in red have a city also)',
+             fontdict={'size':22})
 ax.set_ylabel('Count of venues you can surf, kitesurf and windsurf')
 ax.set_xticks(df_surf["address_region"])
 ax.set_xticklabels(df_surf["address_region"], rotation=60, fontdict={'horizontalalignment': 'right', 'size':12})
+ax.set_xlabel('Counties with surfing venues. Only counties in red have a city')
 ax.set_ylim(0, df_surf["count_venue"].max() + 1)
 plt.show()
-fig.savefig("./data/surf_venue_count_per_region.png")
+fig.savefig("./visualisations/lollipop_chart_surf_activities.png")
 plt.clf()
 
 
-# Draw Stripplot
-fig, ax = plt.subplots(figsize=(16,10), dpi= 80)
-sns.stripplot(df_surf["address_region"], df_surf["count_venue"], ax=ax)
-
-plt.show()
-
-plt.clf()
-
-# Create a regression plot
-#sns.lmplot(data=data.df_surf_per_cnty,
-#           x="address_region",
-#           y="count_venue")
-
-
-# Create a PairGrid with a scatter plot for fatal_collisions and premiums
-g = sns.PairGrid(df_surf, vars=["address_region", "count_venue"])
-g2 = g.map(plt.scatter)
-
-plt.show()
-plt.clf()
-
-# Create a boxplot
-sns.boxplot(data=df_surf,
-         x='address_region',
-         y='count_venue')
-
-plt.show()
-#plt.clf()
-
-# Remove the spines
-sns.despine(top = True, right = True)
-
-# Show the plot and clear the figure
-plt.show()
-plt.clf()
-
-# Create a regression plot using hue
-sns.lmplot(data=df,
-           x="insurance_losses",
-           y="premiums",
-           hue="Region")
-
-# Show the results
-plt.show()
-
-
-
-df_munster = data.df_attractions_munster.sort_values("number_attractions", ascending=True)
-#df_leinster = data.df_attractions_leinste
-fig, ax = plt.subplots()
-# Plot counties from df_munster against the number_attractions there
-ax.plot(df_munster.index, df_munster["number_attractions"], color='b', marker='o', linestyle='None')
-plt.show()
-fig.savefig("./data/df_munster.png")
-
-# Plot counties from df_munster against the number_activites there
-#ax.plot(df_munster["county"], df_munster["number_attractions"])
-#plt.show()
-
-
-df_all_provinces = data.df_attractions_per_cnty
+################
+# For Figure 2: Bar plot subplots plotting number attractions per county for each province
+################
+df_all_provinces = data.df_agg_attractions
 df_leinster = df_all_provinces[df_all_provinces["province"] == "Leinster"].sort_values("number_attractions", ascending=True)
 df_munster = df_all_provinces[df_all_provinces["province"] == "Munster"].sort_values("number_attractions", ascending=True)
 df_connacht = df_all_provinces[df_all_provinces["province"] == "Connacht"].sort_values("number_attractions", ascending=True)
 df_ulster = df_all_provinces[df_all_provinces["province"] == "Ulster"].sort_values("number_attractions", ascending=True)
-# Create a Figure and an array of subplots with 2 rows and 2 columns
-fig, ax = plt.subplots(2, 2)
-# Addressing the top left Axes as index 0, 0, plot month and Seattle precipitation
-ax[0, 0].plot(df_leinster.index, df_leinster["number_attractions"], color='r', marker='o', linestyle='None')
+# Create subplots for each province
+fig, ax = plt.subplots(2, 2, figsize=(16,10), dpi= 80)
 
-# In the top right (index 0,1), plot month and Seattle temperatures
-ax[0, 1].plot(df_munster.index, df_munster["number_attractions"], color='r', marker='o', linestyle='None')
-
-# In the bottom left (1, 0) plot month and Austin precipitations
-ax[1, 0].plot(df_connacht.index, df_connacht["number_attractions"], color='r', marker='o', linestyle='None')
-
-# In the bottom right (1, 1) plot month and Austin temperatures
-ax[1, 1].plot(df_ulster.index, df_ulster["number_attractions"], color='r', marker='o', linestyle='None')
+# Top left - plot count and number_attractions for Leinster
+ax[0,0].bar(df_leinster.index, df_leinster["number_attractions"], color='r')
+ax[0,0].tick_params(labelrotation=30)
+ax[0,0].set_title('Figure 2a: Count of attractions per county in Leinster')
+# Top right - plot count and number_attractions for Munster
+ax[0,1].bar(df_munster.index, df_munster["number_attractions"], color='g')
+ax[0,1].tick_params(labelrotation=30)
+ax[0,1].set_title('Figure 2b: Count of attractions per county in Munster')
+# Bottom left - plot count and number_attractions for Connacht
+ax[1,0].bar(df_connacht.index, df_connacht["number_attractions"], color='b')
+ax[1,0].tick_params(labelrotation=30)
+ax[1,0].set_title('Figure 2c: Count of attractions per county in Connacht')
+# Bottom right - plot count and number_attractions for Ulster
+ax[1,1].bar(df_ulster.index, df_ulster["number_attractions"], color='y')
+ax[1,1].tick_params(labelrotation=30)
+ax[1,1].set_title('Figure 2d: Count of attractions per county in Ulster')
 plt.show()
+fig.savefig("./visualisations/bar_subplots_per_province.png")
+plt.clf()
+#########
+
+fig, ax = plt.subplots()
+df_attractions_leinster = data.df_attractions_leinster
+df_accommodation_leinster = data.df_accommodation_leinster
+# Add data: "co2" on x-axis, "relative_temp" on y-axis
+ax.scatter(df_attractions_leinster.index, df_attractions_leinster["number_attractions"], c = ['r'])
+plt.xticks(rotation='45')
+
+ax.scatter(df_accommodation_leinster.index, df_accommodation_leinster["number_attractions"], c = ['b'])
+plt.xticks(rotation='45')
+
+# Set the x-axis label to "CO2 (ppm)"
+ax.set_xlabel("Counties in Ireland - (AddressRegion)")
+
+# Set the y-axis label to "Relative temperature (C)"
+ax.set_ylabel("Total visitor attractions")
+
+plt.show()
+fig.savefig("./visualisations/scatter_plot_aggregated_counts.png")
 
 
-# attraction with most tags in Dublin
-# county with most activities in each province
+#######
+df_accommodation = data.df_accommodation
+g = sns.displot(df_accommodation, x="AddressRegion", hue = "Province", aspect=50/20)
+g.set_xticklabels(rotation=45)
+#g.set_titles('Figure 3: Seaborn distplot (histogram) of the number of accommodations per "AddressRegion"')
+plt.show()
+plt.savefig("./visualisations/accommodation_per_county.png")
+plt.clf()
+########
 
-#compare percentage of unknown phone numbers per county
-#compare percentage of unknown urls numbers per county
+plt.style.use("ggplot")
+plt.scatter(df_attractions_leinster["number_attractions"], df_accommodation_leinster["number_attractions"])
+plt.show()
+plt.savefig("./visualisations/scatter_plot_of_correlation.png")
+
+
+
 
 a = "end"
-# Plot first column of df
-#pd.DataFrame.hist(df.ix[:, 0:1])
-#plt.xlabel('fixed acidity (g(tartaric acid)/dm$^3$)')
-#plt.ylabel('count')
-#plt.show()
